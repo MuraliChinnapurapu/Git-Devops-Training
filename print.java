b1f4b032fb24702433b3a4c0975bf5e97ced5b84
MuraliChinnapurapu <94052138+MuraliChinnapurapu@users.noreply.github.com>
2021-11-23 15:52:14 +0530

    commit on 23-nov-2021

--- print.java
+++ print.java
@@ -1,12 +1,14 @@
-public class test{
-	public static void main(String[]  args)
-	{
-		System.out.println("Git Test Sample Print");
-
-		System.out.println("Updating the existing file to test git workflow");
-
-		System.out.println("Sample Commit trail on day 2")
-		
-	}
+public class test {
 	
+    public static void main(String[] args) {
+        System.out.println("Git Test Sample Print");
+
+        System.out.println("Updating the existing file to test git workflow");
+
+        System.out.println("Sample Commit trail on day 2")
+
+        System.out.println("Sample Test commit for the third day on 23-nov-2021")
+
+    }
+
 }
